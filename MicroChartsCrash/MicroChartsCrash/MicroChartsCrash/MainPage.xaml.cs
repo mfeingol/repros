f3a933c9005f5f9e04d578d2c220984334a25aaf
Max Feingold <mfeingol@outlook.com>
2020-07-27 08:54:22 -0700

    Add repro for https://github.com/dotnet-ad/Microcharts/issues/193

---
+++ MicroChartsCrash/MicroChartsCrash/MicroChartsCrash/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Microcharts;
+using SkiaSharp;
+
+namespace MicroChartsCrash
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            List<ChartEntry> entries = new List<ChartEntry>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                entries.Add(new ChartEntry(i + 100) { Color = SKColors.Black });
+
+                if (i % 20 == 0)
+                    entries[i].ValueLabel = $"Label {i}";
+            }
+
+            this.Chart = new LineChart
+            {
+                LabelColor = SKColors.LavenderBlush,
+                BackgroundColor = SKColors.Red,
+                Entries = entries,
+                LineMode = LineMode.Straight,
+                PointMode = PointMode.Circle,
+                LabelTextSize = 28,
+            };
+
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+
+        public LineChart Chart { get; }
+    }
+}
