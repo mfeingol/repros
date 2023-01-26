537adf82ef06be88f45a7ddc3014e99eeaedd297
Max Feingold <mfeingol@users.noreply.github.com>
2023-01-25 22:01:07 -0700

    Add CarouselViewRepro

---
+++ CarouselViewRepro/CarouselViewRepro/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CarouselViewRepro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CarouselViewRepro/CarouselViewRepro/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CarouselViewRepro;
+
+public partial class MainPage : ContentPage
+{
+    public MainPageViewModel ViewModel { get; } = new();
+
+    public MainPage()
+	{
+		InitializeComponent();
+        this.BindingContext = this;
+	}
+	
+    void Button_Clicked(object sender, EventArgs e)
+    {
+        int index = this.ViewModel.Items.IndexOf(this.ViewModel.SelectedItem);
+        index = (++index) % this.ViewModel.Items.Count;
+
+        this.ViewModel.SelectedItem = this.ViewModel.Items[index];
+    }
+}
+
+public class MainPageViewModel : Observable
+{
+    public List<string> Items { get; } = new() { "1", "2", "3", "4", "5" };
+
+    string selectedItem = "1";
+    public string SelectedItem
+    {
+        get => this.selectedItem;
+        set => this.Set(ref this.selectedItem, value);
+    }
+}
+
+public abstract class Observable : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected bool Set<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (Equals(storage, value))
+            return false;
+
+        storage = value;
+        this.OnPropertyChanged(propertyName);
+        return true;
+    }
+    
+    public void OnPropertyChanged(string propertyName) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
