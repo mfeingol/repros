38d7ef22ebec77760aac0f70cbb8273b8447281e
Max Feingold <mfeingol@users.noreply.github.com>
2023-01-27 21:11:42 -0700

    Add ImageButtonClipRepro

---
+++ ImageButtonClipRepro/ImageButtonClipRepro/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace ImageButtonClipRepro;
+
+public partial class MainPage : ContentPage
+{
+    public static readonly BindableProperty IsButtonVisibleProperty = BindableProperty.Create(nameof(IsButtonVisible), typeof(bool), typeof(MainPage), defaultValue: false);
+
+    int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		this.BindingContext = this;
+
+		this.Dispatcher.Dispatch(async () =>
+		{
+            await Task.Delay(1000);
+            this.IsButtonVisible = true;
+        });
+	}
+
+    public bool IsButtonVisible
+    {
+        get => (bool)GetValue(IsButtonVisibleProperty);
+        set => SetValue(IsButtonVisibleProperty, value);
+    }
+
+    void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
---
+++ ImageButtonClipRepro/ImageButtonClipRepro/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ImageButtonClipRepro;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
