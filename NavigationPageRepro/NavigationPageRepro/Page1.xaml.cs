6782ebdedee0eb152f347db1a4d5fb0e2e2cfe78
Max Feingold <mfeingol@users.noreply.github.com>
2023-02-07 17:32:25 -0700

    Add NavigationPageRepro

---
+++ NavigationPageRepro/NavigationPageRepro/Page1.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationPageRepro
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new Page2());
+        }
+    }
+}
---
+++ NavigationPageRepro/NavigationPageRepro/Page2.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationPageRepro
+{
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
