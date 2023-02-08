ec929c3eb5fa3be918dfef08504a036c6f3dea4f
Max Feingold <mfeingol@users.noreply.github.com>
2023-02-07 22:07:51 -0700

    Add CheckboxBindingRepro

---
+++ CheckboxBindingRepro/CheckboxBindingRepro/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace CheckboxBindingRepro;
+
+public partial class MainPage : ContentPage
+{
+    public static readonly BindableProperty IsCheckedProperty = BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(MainPage), defaultValue: false);
+    public static readonly BindableProperty ItemsProperty = BindableProperty.Create(nameof(Items), typeof(IList<MainPage>), typeof(MainPage));
+
+    public MainPage()
+	{
+		InitializeComponent();
+        this.BindingContext = this;
+
+        this.Items = new[] { this };
+    }
+
+    public bool IsChecked
+    {
+        get => (bool)GetValue(IsCheckedProperty);
+        set => SetValue(IsCheckedProperty, value);
+    }
+
+    public IList<MainPage> Items
+    {
+        get => (IList<MainPage>)GetValue(ItemsProperty);
+        set => SetValue(ItemsProperty, value);
+    }
+}
