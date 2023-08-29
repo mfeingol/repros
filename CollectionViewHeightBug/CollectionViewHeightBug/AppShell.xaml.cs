c17ac92f6c02ee9fccfc5c74089ee36c6a18a1f4
Max Feingold <mfeingol@outlook.com>
2023-08-28 21:53:21 -0700

    Add CollectionViewHeightBug

---
+++ CollectionViewHeightBug/CollectionViewHeightBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionViewHeightBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CollectionViewHeightBug/CollectionViewHeightBug/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CollectionViewHeightBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<object> Items { get; set; } = new()
+        {
+            new(),
+            new(),
+            new(),
+            new(),
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+    }
+}
