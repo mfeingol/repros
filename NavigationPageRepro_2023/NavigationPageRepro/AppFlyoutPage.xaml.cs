886f43fd3d3dad34beb43dac62e0e4a540d75037
Max Feingold <mfeingol@users.noreply.github.com>
2023-12-06 12:08:06 -0700

    Update NavigationPageRepro_2023

--- NavigationPageRepro_2023/NavigationPageRepro/AppFlyoutPage.xaml.cs
+++ NavigationPageRepro_2023/NavigationPageRepro/AppFlyoutPage.xaml.cs
@@ -5,12 +5,19 @@ public partial class AppFlyoutPage : FlyoutPage
 	public AppFlyoutPage()
 	{
 		InitializeComponent();
-		this.appFlyoutPageMenu.FlyoutPage = this;
+		this.Menu.FlyoutPage = this;
     }
 
 	public void NavigateTo(Type type)
 	{
-        this.Detail = new NavigationPage((Page)Activator.CreateInstance(type));
+        NavigationPage nav = new((Page)Activator.CreateInstance(type));
+
+        // This doesn't work, and it's a regression from Xamarin Forms
+        //this.Detail = nav;
+
+        // This is the workaround I've found
+        App.Current.MainPage = new AppFlyoutPage { Detail = nav };
+
         if (!((IFlyoutPageController)this).ShouldShowSplitMode)
             this.IsPresented = false;
     }
--- NavigationPageRepro_2023/NavigationPageRepro/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NavigationPageRepro;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
