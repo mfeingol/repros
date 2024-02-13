52b27cd1cb6166139bde4b520fa4d1820cf3accf
Max Feingold <mfeingol@users.noreply.github.com>
2024-02-12 21:07:04 -0700

    Add LotsOfPolylinesRepro_XF for https://github.com/dotnet/maui/issues/20502

---
+++ LotsOfPolylinesRepro_XF/LotsOfPolylinesRepro_XF/LotsOfPolylinesRepro_XF/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LotsOfPolylinesRepro_XF.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LotsOfPolylinesRepro_XF
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MapPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
