369c142b5b23325c9a3bcd69ec02c6f6a8898bd2
Max Feingold <mfeingol@users.noreply.github.com>
2024-04-18 18:34:30 -0600

    Add BackgroundColorCrashRepro

---
+++ BackgroundColorCrashRepro/BackgroundColorCrashRepro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BackgroundColorCrashRepro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
